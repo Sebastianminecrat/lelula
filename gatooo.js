5f61d3e7cf76d721ce987864e014e5bcd5ce6950
Sebastianminecrat <135288080+Sebastianminecrat@users.noreply.github.com>
2023-06-05 19:44:44 -0600

    Add files via upload

---
+++ gatooo.js
@@ -0,0 +1,8 @@
+function panamal(){user_name=document.getElementById("wakanda come salchichas").value;
+localStorage.setItem("wakanda come salchichas",user_name);
+window.location="gatodehierro.html"
+
+
+
+
+}
